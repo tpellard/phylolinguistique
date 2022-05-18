1fc53c745ea4b6860296b6e7dc22004da7ac320f
tpellard <thomas.pellard@cnrs.fr>
2022-05-18 15:36:17 +0200

    Update _quarto.yml, bibliographie.html, and 17 more files...

--- make_logo.R
+++ make_logo.R
@@ -2,7 +2,13 @@ library(tidyverse)
 library(ggtree)
 set.seed(1234567890)
 tree <- rtree(33)
-ggtree(tree, branch.length='none', layout='circular', color = "#adb5bd", size = 1) +
+
+ggtree(tree, branch.length = "none", layout = "circular", color = "#adb5bd", size = .5) +
   theme_void()
-ggsave("phyloling.svg", width = 400, height = 400, units = "px")
-ggsave("phyloling.png", width = 400, height = 400, units = "px")
+ggsave("phyloling_circ.png", width = 300, height = 300, units = "px", dpi = 300)
+knitr::plot_crop("phyloling_circ.png")
+
+ggtree(tree, branch.length = "none", layout = "fan", color = "#adb5bd", size = .25, open.angle = 180) +
+  theme_void()
+ggsave("phyloling_scirc.png", width = 150, height = 150, units = "px", dpi = 300)
+knitr::plot_crop("phyloling_scirc.png")
